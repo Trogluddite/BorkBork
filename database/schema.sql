66e15398e7a6cfbfe86b36324bb72dca3318ea3c
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2025-05-23 13:53:13 -0700

    iterating on schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,22 +1,39 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS room_membership;
 DROP TABLE IF EXISTS chat_history;
 
 CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  username TEXT NOT NULL,
-  email TEXT NOT NULL,
+  username VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
   email_verified BIT NOT NULL,
-  role TEXT NOT NULL,
-  password_hash INTEGER,
-  FOREIGN KEY (id) REFERENCES rooms(id)
-
+  role VARCHAR NOT NULL,
+  password_hash INTEGER
 );
 
-CREATE TABLE rooms (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE room (
+  room_id INTEGER PRIMARY KEY AUTOINCREMENT,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  room_name TEXT NOT NULL,
+  room_name VARCHAR NOT NULL,
   is_public BIT NOT NULL
 );
+
+CREATE TABLE room_membership (
+  user_id INTEGER,
+  room_id INTEGER,
+  PRIMARY KEY (user_id, room_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id),
+  FOREIGN KEY (room_id) REFERENCES room(room_id)
+);
+
+CREATE TABLE chat_history(
+  message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  message_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  message_text TEXT,
+  sender_id INTEGER NOT NULL,
+  room_id INTEGER NOT NULL,
+  FOREIGN KEY (room_id) REFERENCES room(room_id)
+);
+
