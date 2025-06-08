dab69594f7d95ce4adb5854e8e047272d97a231b
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2025-06-07 17:27:49 -0700

    replace println w/ logging in app.rs

--- client-bork/src/app.rs
+++ client-bork/src/app.rs
@@ -1,3 +1,4 @@
+use log::{info, error};
 use std::io::Read;
 use std::net::{Shutdown, TcpStream};
 use socket2::{Socket, Domain, Type};
@@ -47,21 +48,23 @@ impl App{
         self.server_port = port;
         let address = format!("{}:{}", self.server_address, self.server_port);
         let stream = TcpStream::connect(&address).map_err(|err|{
-            eprintln!("Couldn't connect, error was: {}", err);
+            error!("Couldn't connect, error was: {}", err);
         });
         self.tcpstream = stream.unwrap();
         self.server_connected = true;
+        info!("Connected to server {}:{}", ip, port);
     }
 
     pub fn disconnect(&mut self){
         match self.tcpstream.shutdown(Shutdown::Both) {
-            Err(e) => eprintln!("failed to shutdown TCPStream, with Err: {}", e),
+            Err(e) => error!("failed to shutdown TCPStream, with Err: {}", e),
             _ => ()
         };
         self.server_major_ver = 0;
         self.server_minor_ver = 0;
         self.server_subminor_ver = 0;
         self.server_connected = false;
+        info!("disconnected");
     }
 
     pub fn switch_screen(&mut self, target: CurrentScreen){
@@ -76,7 +79,7 @@ impl App{
             // first byte is the message type per protocol -- read it, handle based on type
             let mut mtype = [0u8];
             match self.tcpstream.read_exact(&mut mtype) {
-                Err(e) => eprintln!("failed to read message type, with Err: {}", e),
+                Err(e) => error!("failed to read message type, with Err: {}", e),
                 _ => ()
             }
             match mtype[0] {
@@ -85,15 +88,15 @@ impl App{
                     let mut minor = [0u8];
                     let mut subminor = [0u8];
                     match self.tcpstream.read_exact(&mut major) {
-                        Err(e) => eprintln!("Failed to read major version with Err: {}", e),
+                        Err(e) => error!("Failed to read major version with Err: {}", e),
                         _ => ()
                     }
                     match self.tcpstream.read_exact(&mut minor) {
-                        Err(e) => eprintln!("Failed to read minor version with Err: {}", e),
+                        Err(e) => error!("Failed to read minor version with Err: {}", e),
                         _ => ()
                     }
                     match self.tcpstream.read_exact(&mut subminor) {
-                        Err(e) => eprintln!("Failed to read subminor version with Err: {}", e),
+                        Err(e) => error!("Failed to read subminor version with Err: {}", e),
                         _ => ()
                     }
                     self.server_major_ver = u8::from_le_bytes(major);
@@ -105,13 +108,13 @@ impl App{
                     // and read that many bytes
                     let mut len = [0u8, 0u8];
                     match self.tcpstream.read_exact(&mut len){
-                        Err(e) => eprintln!("failed to read Welcome message length with Err: {}", e),
+                        Err(e) => error!("failed to read Welcome message length with Err: {}", e),
                         _ => ()
                     }
                     let len:u16 = u16::from_le_bytes(len);
                     let mut wm_buf = vec![0; len as usize];
                     match self.tcpstream.read_exact(&mut wm_buf){
-                        Err(e) => eprintln!("failed to read Welcome message content with Err: {}", e),
+                        Err(e) => error!("failed to read Welcome message content with Err: {}", e),
                         _ => ()
                     }
                     self.inbuffer.extend_from_slice(&wm_buf[0..]);
